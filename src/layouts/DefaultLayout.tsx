73241f861b34edb50d00730307e329ac7b9de914
Renan Vilela <renanvilelati@gmail.com>
2023-04-06 10:03:26 -0300

    Home page done

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import { Header } from '../components/Header'
 import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from './styles'
 
 export const DefaultLayout = () => {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   )
 }
